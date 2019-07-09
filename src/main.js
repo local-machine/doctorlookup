7d96789fd96258fea149a34a9c9375cdffaca438
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-07-08 22:34:22 -0700

    Made three input types for different ways to search doctor and began implimenting API in main.js

--- src/main.js
+++ src/main.js
@@ -3,27 +3,45 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Constructor-Name } from './backend-code';
-var apiKey = require('./../.env').exports.apiKey;
+// var apiKey = require('./../.env').exports.apiKey;
+
 
 $(function() {
-  $('#doctorList').click(function() {
-    let city = $('#location').val();
-    $('#location').val("");
-    $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors/?query=${city}&user_key= [API-KEY-GOES-HERE]`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.showName').text(`Doctor Name: ${response.data.profile.first_name}%` + " " + `${response.data.profile.last_name}%`);
-        $('.showAddress').text(`Address: ${response.data.practices[0].visit_address_street}%` + " " + `${response.data.practices[0].visit_address_street2}%` + " " + `${response.data.practices[0].visit_address.zip}%`);
-        // $('.showWebsite').text(`Website: ${response.data.practice.website}.`);
-        $('.showAcceptingNewPatients').text(`Accepting New Patients: ${response.data.practice[0].accepts_new_patients}%`);
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.");
-      }
-    });
+  $('#searchNameForm').on('submit', (event) => {
+    let searchNameInput = $('#searchNameInput').val();
+    getDoctorsName(searchNameInput);
+    event.preventDefault();
   });
+
+
+function getDoctorsName(searchNameInput){
+  console.log(searchNameInput);
+  axios.get(`https://api.betterdoctor.com/2016-03-01/doctors/?query=${city}&user_key=[API-KEY-GOES-HERE]` + searchNameInput)
+  .then((response) => {
+    console.log();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+//   $('#doctorList').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+//     $.ajax({
+//       url: `https://api.betterdoctor.com/2016-03-01/doctors/?query=${city}&user_key= [API-KEY-GOES-HERE]`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         $('.showName').text(`Doctor Name: ${response.data.profile.first_name}` + " " + `${response.data.profile.last_name}%`);
+//         $('.showAddress').text(`Address: ${response.data.practices[0].visit_address_street}` + " " + `${response.data.practices[0].visit_address_street2}` + " " + `${response.data.practices[0].visit_address.zip}`);
+//         // $('.showWebsite').text(`Website: ${response.data.practice.website}.`);
+//         $('.showAcceptingNewPatients').text(`Accepting New Patients: ${response.data.practice[0].accepts_new_patients}`);
+//       },
+//       error: function() {
+//         $('#errors').text("There was an error processing your request. Please try again.");
+  //     }
+  //   });
+  // });
 });
